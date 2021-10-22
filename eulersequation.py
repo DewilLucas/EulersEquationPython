608efe5abe82f19304e2a5e194c49349efe0cd82
Lucas Dewil <91437297+DewilLucas@users.noreply.github.com>
2021-10-22 21:10:38 +0200

    Add files via upload

---
+++ eulersequation.py
@@ -0,0 +1,26 @@
+# Dewil Lucas,22/10/2021
+# I was chilling on Youtube and then sudenly I saw a video : https://www.youtube.com/watch?v=IUTGFQpKaPU
+# This video was talking about the most beatiful equation in mathematics.
+# The mathematician who explained the equation
+import math
+# importing "cmath" for complex number operations
+import cmath
+
+# Initializing real numbers
+x = -1.0
+y = 0.0
+
+# converting x and y into complex number
+i = complex(x, y)
+
+# Complex numbers involve a lot of calculation with the square root from -1.
+# This is also called the number i.
+# But, if i is the root from -1, then the square of i will equal -1.
+
+# Pi
+PI = math.pi
+# making e
+E = math.e
+calc = E**(PI*i)
+calc2 = calc + 1
+print(calc2.imag)
